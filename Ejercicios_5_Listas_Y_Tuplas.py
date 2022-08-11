546778935f63664dd7312c572c19eac888008615
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-08-11 06:55:51 -0500

    Update Ejercicios_5_Listas_Y_Tuplas.py

--- Ejercicios_5_Listas_Y_Tuplas.py
+++ Ejercicios_5_Listas_Y_Tuplas.py
@@ -21,29 +21,72 @@ Ejercicio 3
 Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada 
 asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las 
 correspondientes notas introducidas por el usuario.
+
+Materias = ["Matematicas", "Fisica", "Quimica", "Historia", "Lengua"]
+Total = []
+for i in range(0,(len(Materias))):
+    Nota = int(input(f'¿Que nota sacaste en {Materias[i]}? '))
+    Total.append(Nota)
+for j in range(len(Materias)):
+    print(f'Sacaste en {Materias[j]}: {Total[j]}')
 """
 
 """
 Ejercicio 4
 Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+
+Total = int(input("¿Cuantos numeros tiene la loteria?  \n"))
+Ganadores = []
+for i in range(Total):
+    Loteria = int(input("Voletos ganadores: "))
+    Ganadores.append(Loteria)
+    Ganadores.sort()
+print(Ganadores)
 """
+
 """
 Ejercicio 5
 Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+Numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Numeros.reverse()
+print(Numeros)
 """
+
 """
 Ejercicio 6
 Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada 
 asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+
+Materias = ["Matematicas", "Fisica", "Quimica", "Historia", "Lengua"]
+Total = []
+for i in range(0,(len(Materias))):
+    Nota = int(input(f'¿Que nota sacaste en {Materias[i]}? '))
+    Total.append(Nota)
+for j in range(len(Materias)):
+    if Total[j] <= 5 and Total[j]>= 0:
+        print(f'Sacaste en {Materias[j]}: {Total[j]} Materia por Repetir')
+    else: 
+        print(f'Sacaste en {Materias[j]}: {Total[j]} Materia Pasada')
 """
+
 """
 Ejercicio 7
 Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
+
+abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(0, len(abecedario), 3):
+    if i % 3 == 0:
+        print(abecedario[i].split(','), end=" ")
 """
 """
 Ejercicio 8
 Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo.
+Palindromos " Son palabras que son iguales de derecha a izquierda y de izquierda a derecha, ejemplo= "radar, oso, reconocer, ala"
 """
+Palabra = input("Palindromo: ")
+for i in range(len(Palabra)):
+    print(Palabra[i])
 """
 Ejercicio 9
 Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal.
