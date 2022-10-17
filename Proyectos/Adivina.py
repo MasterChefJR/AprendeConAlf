c77a8f21718b6a53f6809769a8ce5130ad4e1a37
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-16 20:47:17 -0500

    Update Adivina.py

--- Proyectos/Adivina.py
+++ Proyectos/Adivina.py
@@ -6,15 +6,18 @@ Aleatorio = random.randrange(0,20)
 #     if  num == Aleatorio:
 #         break
 # print(f'Acertaste el numero es {Aleatorio}')
-print(f'El numero es ¿{Aleatorio}?')
-Adivina = int(input("1.Si\n2.No\n"))
-x = True
-if Adivina == 2:
-    while (Aleatorio != Adivina) == x:
-        Aleatorio = random.randrange(0,20)
-        print(f'El numero es {Aleatorio}')
-        Adivina = int(input("1.Si\n2.No\n"))
-        if Adivina == 1:
-            x = False
-            break
-print(f'Acerte')
+
+
+# La Computadora Adivina el numero
+# print(f'El numero es ¿{Aleatorio}?')
+# Adivina = int(input("1.Si\n2.No\n"))
+# x = True
+# if Adivina == 2:
+#     while  x:
+#         Aleatorio = random.randrange(0,20)
+#         print(f'El numero es ¿{Aleatorio}?')
+#         Adivina = int(input("1.Si\n2.No\n"))
+#         if Adivina == 1:
+#             x = False
+# print(f'Acerte')
+
