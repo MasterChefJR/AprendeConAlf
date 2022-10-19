a93505e8f900e2fbc19eacfd81f6d8f2c01412b5
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-18 21:36:33 -0500

    .

--- Proyectos/Adivina_La_Palabra.py
+++ Proyectos/Adivina_La_Palabra.py
@@ -1,14 +1,3 @@
-# file = open("Adivina.txt", "r")
-# cont = file.read()
-# print(cont)
-# file.close()
-
-
-# with open('Adivina.txt') as a:
-#     contenido = a.read()
-#     print(contenido)
-
-f = open ('Adivina.txt','r')
-mensaje = f.read()
-print(mensaje)
-f.close()
+with open('adibina.txt') as a:
+    contenido = a.read()
+    print(contenido)
