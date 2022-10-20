8aed8e7a2e77e9b01637c7323db5efc0791072fe
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-20 18:06:44 -0500

    .

--- Proyectos/Adivina_La_Palabra.py
+++ Proyectos/Adivina_La_Palabra.py
@@ -1,16 +1,42 @@
 import random
-
-def hello():
-    with open("adibina.txt") as a:
-        p = a.read()
-        return p
-print(hello())
+# def hello():
+#     with open("animales.txt") as a:
+#         p = a.read()
+#         return p
+x = random.randrange(0,20)
+animales = ["hog", "mole", "puppy", "beaver" ,"cow" , "kangaroo" , "koala" , "oryx", "lynx", "anteater", "parakeet", "aardvark", "squirrel", "elk", "crow", "gila monster", "pony", "horse", "musk deer", "walrus"]
+z = (animales[x])
 x = True
+for i in z:
+    print(i,end=",")
+print(z[0:len(z)])
+palabra = "Hola Mundo"
 while x:
-    palabra = input("Escribe una letra")
-    if len(palabra) >1:
-        print("Solo una letra")
-    elif len(palabra) == 1:
-        print(f'Elejiste {palabra}')
+    c = 6
+    nombre = input("Escribe la palabra clave:")
+    
+    if nombre != palabra:
+        c = c-1
+        print(f'Te quedan {c}')
+    if len(nombre) >1:
         x = False
-print(f'acertaste la palabra es {palabra}')        
+
+# while x:
+#     palabra = input("Adivina el nombre del animal\nEscribe una letra: ")
+#     contador = 0
+#     vidas = 7
+#     if (palabra != z[0]):
+#         vidas -=1
+
+#     elif vidas == 0:
+#         print("Perdiste")
+#         x = False
+#     print(f'Te quedan {vidas} vidas')
+
+#     if len(palabra) >1:
+#         print("Solo una letra")
+#         x = False
+#     if palabra == z[contador]:
+#         print(f'La palabra es {z[i]}')
+#         contador +=1
+# print(f'acertaste la palabra es {palabra}')        
