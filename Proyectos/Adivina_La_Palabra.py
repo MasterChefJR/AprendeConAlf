c425ece30c1bf345ad231304c2b64368c8370f21
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-20 13:19:34 -0500

    .

--- Proyectos/Adivina_La_Palabra.py
+++ Proyectos/Adivina_La_Palabra.py
@@ -1,3 +1,16 @@
-with open('adibina.txt') as a:
-    contenido = a.read()
-    print(contenido)
+import random
+
+def hello():
+    with open("adibina.txt") as a:
+        p = a.read()
+        return p
+print(hello())
+x = True
+while x:
+    palabra = input("Escribe una letra")
+    if len(palabra) >1:
+        print("Solo una letra")
+    elif len(palabra) == 1:
+        print(f'Elejiste {palabra}')
+        x = False
+print(f'acertaste la palabra es {palabra}')        
