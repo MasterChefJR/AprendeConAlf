50863e1d4d5d67f9daf58381cae8317580229ee0
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-22 03:38:59 -0500

    .

--- Proyectos/Adivina_El_Numero.py
+++ Proyectos/Adivina_El_Numero.py
@@ -1,11 +1,15 @@
-#El usuario adivina el numero
-# import random
-# Aleatorio = random.randrange(0,20)
-# num = int(input("adivina el numero del 0 al 20: "))
-# while (num != Aleatorio):
-#     num = int(input("Inserta un numero: "))
-#     if  num == Aleatorio:
-#         break
-# print(f'Acertaste el numero es {Aleatorio}')
+# El usuario adivina el numero
+import random
+Aleatorio = random.randrange(0,20)
+x = True
+while x:
+    num = int(input("Inserta un numero: "))
+    if  num == Aleatorio:
+        print(f'Acertaste el numero es {Aleatorio}')
+        break
+    elif num > Aleatorio:
+        print("Alto")
+    elif num < Aleatorio:
+        print("Bajo")
 
 
--- Proyectos/Adivina_La_Palabra.py
+++ Proyectos/Adivina_La_Palabra.py
@@ -1,32 +1,30 @@
 import random
+# Tabla para los animales
 # def hello():
 #     with open("animales.txt") as a:
 #         p = a.read()
 #         return p
-# DECLARAMOS LA LISTA DE LOS ANIMALES
-x = random.randrange(0,20)
+# DECLARAMOS LA LISTA DE LOS ANIMALES Y SELECCIONAMOS AL ANIMAL
+x = random.randint(0,20)
 animales = ["hog", "mole", "puppy", "beaver" ,"cow" , "kangaroo" , "koala" , "oryx", "lynx", "anteater", "parakeet", "aardvark", "squirrel", "elk", 
 "crow", "gila monster", "pony", "horse", "musk deer", "walrus"]
-z = (animales[x])
-# vISUALISAMOS LOS ANIMALES QUE SALGAN
-for x in z:
-    print(x,end=",")
-print(z[0:len(z)])
-nombre = ''
+animal = (animales[x])
+largo = len(animal)
+print(f'animal: {animal}\nPalabras: {largo}')
 # ADIVINAMOS LA PALABRA DEL ANIMAL
-x = True
-while x:
-    c = 7
-    if nombre != z[x]:
-        for i in range(0,7):
-            if nombre == z[x]:
-                print(f'Correcto {nombre} Tienes {c} vidas')
-            elif nombre != z[x]:
-                c = c-1
-            nombre = input("Escribe la palabra clave:")
-            print(f'Te quedan {c} vidas')
-    if c == 0 or len(nombre) >1:
-        x = False
+    # continue
+Vidas = 7
+for i in range(0,Vidas):
+    for x in range(0,largo):
+        print(animal[x])
+    nombre = input(f'Tienes {Vidas} Vidas Escribe Una Letra\n')
+    if Vidas == 0 or len(nombre) >1:
+        break
+    elif nombre[0] != animal[x]:
+        Vidas = Vidas-1
+        print(f'Tienes {Vidas} Vidas Vuelve a intentarlo')
+    elif nombre[0] == animal[x]:
+        print(animal[x]+" s")
 
 # while x:
 #     palabra = input("Adivina el nombre del animal\nEscribe una letra: ")
@@ -46,4 +44,4 @@ while x:
 #     if palabra == z[contador]:
 #         print(f'La palabra es {z[i]}')
 #         contador +=1
-# print(f'acertaste la palabra es {palabra}')        
+# print(f'acertaste la palabra es {palabra}')
