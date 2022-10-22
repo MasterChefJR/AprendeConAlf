23fdd116b89b2523d7240a4e318cdfd6b8111394
MasterChefJR <diosmirrabitcoin@gmail.com>
2022-10-22 10:58:48 -0500

    .

--- DataScience/Help.py
+++ DataScience/Help.py
@@ -1,6 +1,8 @@
-x = [1,2,3]
-y = [3,2,1]
-z = x+y
-total = []
-for i in z:
-    print(i)
+# # Suma De Arrays
+# x = [10,20,10]
+# y = [10,20,10]
+# z = [0]
+# for i in range(0,3):
+#     z[0] += x[i] + y[i]
+# print(z)
+
