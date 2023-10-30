12b89fdbf7b48c9cf22e5e942d3a945b6ec542fa
MasterChefJR <diosmirrabitcoin@gmail.com>
2023-10-30 06:10:21 -0600

    .

---
+++ AprendeConAlf/hackerank/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        Scanner n = new Scanner(System.in);
+        String name = n.nextLine();
+        System.out.println(name);
+    }
+}
